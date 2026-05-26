39b5ca040fe2a79e1b35925472a92173e6afd0d4
DaviJesus66 <davi.jesus@iesb.edu.br>
2026-05-26 18:12:31 -0300

    vht

---
+++ praticas/pomodoro/chronos-pomodoro/src/contexts/TaskContext/TaskContext.tsx
@@ -0,0 +1,15 @@
+import { createContext, type Dispatch } from 'react';
+import type { TaskStateModel } from '../../models/TaskStateModel';
+import type { TaskActionModel } from './taskActions';
+
+type TaskContextProps = {
+  state: TaskStateModel;
+  dispatch: Dispatch<TaskActionModel>;
+};
+
+const initialContextValue = {
+  state: initialTaskState,
+  dispatch: () => {},
+};
+
+export const TaskContext = createContext<TaskContextProps>(initialContextValue);
--- praticas/projeto_react/chronos-pomodoro/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import {Home} from './pages/Home';
-
-import './styles/thema.css';
-import './styles/global.css';
-
-export function App() {
-  return <Home />;
-}
